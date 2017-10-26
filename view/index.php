1f3d2612b2762961d32e0db287ee5cd604b0d2ee
benjamin <degroote.benjamin@gmail.com>
2017-10-26 09:29:53 +0200

    create view for index and already create the table and the foreach

---
+++ view/index.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row">
+        <table class="table table-responsive d-flex justify-content-center">
+            <tr class="thead-inverse">
+                <th>brand</th>
+                <th>name</th>
+                <th>create_date</th>
+                <th>type</th>
+                <th>update</th>
+                <th>delete</th>
+            </tr>
+            <?php foreach ($vehicle as $key => $vehiclevalue) {
+              ?>
+            <tr>
+                <td>
+                    <?php echo $vehiclevalue->brand ?>
+                </td>
+                <td>
+                    <?php echo $vehiclevalue->name ?>
+                </td>
+                <td>
+                    <?php echo $vehiclevalue->create_date ?>
+                </td>
+                <td>
+                    <?php echo $vehiclevalue->type ?>
+                </td>
+                <td><a href="controle/index.php?updateid=<?php echo $vehiclevalue->id ?>"><i class="fa fa-wrench" aria-hidden="true"></i></a></td>
+                <td><a href="controle/index.php?deleteid=<?php echo $vehiclevalue->id ?>"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
+            </tr>
+
+            <?php
+            } ?>
+
+        </table>
+    </div>
+</div>
