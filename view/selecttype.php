6dfeee31faf93f7de53feab1bd59feb91d756362
benjamin <degroote.benjamin@gmail.com>
2017-10-26 10:20:57 +0200

    create view for select the type of vehicle and form for add

---
+++ view/selecttype.php
@@ -0,0 +1,28 @@
+<div class="container">
+  <div class="card-deck">
+  <div class="card">
+    <a href="addform.php?type=car">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-footer">
+      <small class="text-muted">Car</small>
+    </div>
+    </a>
+  </div>
+  <div class="card">
+    <a href="addform.php?type=bike">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-footer">
+      <small class="text-muted">bike</small>
+    </div>
+    </a>
+  </div>
+  <div class="card">
+    <a href="addform.php?type=truck">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-footer">
+      <small class="text-muted">truck</small>
+    </div>
+    </a>
+  </div>
+</div>
+</div>
