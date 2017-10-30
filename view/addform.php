9c699f4d7f50624b874e4379a693e5e0f43dd4bd
benjamin <degroote.benjamin@gmail.com>
2017-10-30 16:23:53 +0100

    correct some css error

--- view/addform.php
+++ view/addform.php
@@ -2,7 +2,9 @@
 include 'header.php';
  ?>
  <div class="container d-flex justify-content-center">
-   <a href="add.php"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a><br>
+   <div class="col-1">
+   <a href="add.php"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+      </div>
 <form method="post" action="index.php">
    <div class="form-group">
      <label for="brand">Brand</label>
--- view/update.php
+++ view/update.php
@@ -2,23 +2,24 @@
 include 'header.php';
  ?>
  <div class="container d-flex justify-content-center">
-   <a href="index.php"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a><br>
+   <div class="col-1">
+   <a href="index.php" class="col-12"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+</div>
 <form method="post" action="index.php">
    <div class="form-group">
      <label for="brand">Brand</label>
      <select class="" name="brand" id="brand">
      <?php foreach ($vehicle->type::brand as $key => $value) {
-       if($vehicle->brand == $value){
-                   ?>
+     if ($vehicle->brand == $value) {
+         ?>
       <option value="<?php echo $value; ?>" selected><?php echo $value; ?></option>
        <?php
-     }
-       else {
+     } else {
          ?>
          <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
          <?php
-       }
-     } ?>
+     }
+ } ?>
 
    </select>
    </div>
@@ -30,17 +31,16 @@ include 'header.php';
      <label for="color">color</label>
      <select class="" name="color" id="color">
      <?php foreach (vehicle::color as $key => $value) {
-       if($vehicle->color == $value){
-                   ?>
+     if ($vehicle->color == $value) {
+         ?>
       <option value="<?php echo $value; ?>" selected><?php echo $value; ?></option>
        <?php
-     }
-       else {
+     } else {
          ?>
          <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
          <?php
-       }
-     } ?>
+     }
+ } ?>
    </select>
    </div>
    <?php
@@ -50,58 +50,55 @@ include 'header.php';
           <label for="door">door</label>
           <select class="" name="door" id="door">
           <?php foreach (car::door as $key => $value) {
-            if($vehicle->door== $value){
-                        ?>
+                  if ($vehicle->door== $value) {
+                      ?>
            <option value="<?php echo $value; ?>" selected><?php echo $value; ?></option>
             <?php
-          }
-            else {
-              ?>
+                  } else {
+                      ?>
               <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
               <?php
-            }
-          } ?>
+                  }
+              } ?>
         </select>
         </div>
         <?php
-      } elseif ($vehicle->type =="bike") {
+          } elseif ($vehicle->type =="bike") {
               ?>
   <div class="form-group">
     <label for="helmet">helmet</label>
     <select class="" name="helmet" id="helmet">
     <?php foreach (bike::helmet as $key => $value) {
-      $value1 = ($value) ? 'true' : 'false';
-      if($vehicle->helmet == $value){
-                  ?>
+                  $value1 = ($value) ? 'true' : 'false';
+                  if ($vehicle->helmet == $value) {
+                      ?>
      <option value="<?php echo $value; ?>" selected><?php echo $value1; ?></option>
       <?php
-    }
-      else {
-        ?>
+                  } else {
+                      ?>
         <option value="<?php echo $value; ?>"><?php echo $value1; ?></option>
         <?php
-      }
-    } ?>
+                  }
+              } ?>
   </select>
   </div>
   <?php
-} elseif ($vehicle->type =="truck") {
+          } elseif ($vehicle->type =="truck") {
               ?>
 <div class="form-group">
   <label for="speed">max_speed</label>
   <select class="" name="maxSpeed" id="speed">
   <?php foreach (truck::speed as $key => $value) {
-    if($vehicle->max_speed == $value){
-                ?>
+                  if ($vehicle->max_speed == $value) {
+                      ?>
    <option value="<?php echo $value; ?>" selected><?php echo $value; ?></option>
     <?php
-  }
-    else {
-      ?>
+                  } else {
+                      ?>
       <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
       <?php
-    }
-  } ?>
+                  }
+              } ?>
 </select>
 </div>
 <?php
